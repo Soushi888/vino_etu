f9cc509e5ba29254385de17d36fe5c6b90b55562
enix426 <58696683+enix426@users.noreply.github.com>
2020-09-09 00:38:15 -0400

    creation de pageAuthentification.blade.php, creation de css et d<un import css au nom de pageAuthentification.css première ébauche de la vue authentification

--- resources/views/pageAuthentification.blade.php
+++ resources/views/pageAuthentification.blade.php
@@ -3,20 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/app.css">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
+        <title>Vino</title>
         
     </head>
     <body>
-        <main>
-            <input type="text">
-            <input type="text">
-            <input type="text">
+        <main id="pageAuthentification-main">
+            <img src="img/logo_vino.png" alt="">
+            <form action="">
+                <input type="text" placeholder="Identifiant">
+                <input type="password" placeholder="Mot de passe">
+                <button id="bouton-bleu-main">Entrer</button>
+                <button id="bouton-rouge-main">S'inscrire</button>
+            </form>
         </main>
     </body>
 </html>
