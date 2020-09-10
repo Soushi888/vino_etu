d1aa17822c0aa4f1766906be225adfc88ffbf648
Soushi888 <Sacha.pignot@gmail.com>
2020-09-10 16:51:50 -0400

    Début API SAQ + images ajoutée aux données de test de la table bouteilles (#16)
    En binôme avec @MI7QC .
    
    * ajout d'images aux données de test de la table bouteille
    
    * Update README.md
    
    * Ajout du schema de la bdd en .mwd
    
    * Debut de l'API SAQ
    
    Co-authored-by: Soushi888 <info@sacha-pignot.com>

--- database/factories/BouteilleFactory.php
+++ database/factories/BouteilleFactory.php
@@ -8,13 +8,13 @@ use Faker\Generator as Faker;
 $factory->define(Bouteille::class, function (Faker $faker) {
     return [
         "nom" => $faker->name,
-        "image" => $faker->url,
+        "image" => "https://www.saq.com/media/catalog/product/1/0/10324623-1_1584374899.png",
         "code_saq" => $faker->bankAccountNumber,
         "pays" => $faker->country,
         "description" => $faker->paragraphs(2, true),
         "prix_saq" => $faker->randomNumber(2),
-        "url_saq" => $faker->url,
-        "image_url" => $faker->url,
+        "url_saq" => "https://www.saq.com/page/fr/saqcom/vin-rouge/borsao-seleccion/10324623",
+        "image_url" => "https://www.saq.com/media/catalog/product/1/0/10324623-1_1584374899.png",
         "format" => $faker->words(2, true),
         "type_id" => $faker->numberBetween(1, 2)
     ];
