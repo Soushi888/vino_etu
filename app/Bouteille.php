dc29142b2e4e38382ce52eef68ab1b513907c51e
samuel <54377301+MI7QC@users.noreply.github.com>
2020-09-09 23:22:35 -0400

    api route correction model (#9)
    * Ajout des routes api
    
    * Api bouteille
    
    * Api bouteille et correction du model

--- app/Bouteille.php
+++ app/Bouteille.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bouteille extends Model
 {
-    //
+    // $fillable donne l'authorisation d'envoyer en groupe les données
+    protected $fillable = [
+        'nom', 'image', 'code_saq', "pays",  "description", 'prix_saq', 'url_saq', "image_url","format","type_id"
+    ];
 }
