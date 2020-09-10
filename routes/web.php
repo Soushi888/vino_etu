2f34ee268cddfc5406f734dded959dae6ce22f75
enix426 <58696683+enix426@users.noreply.github.com>
2020-09-10 13:26:10 -0400

    mise a jour de la page login avec les redirections

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('pageAuthentification');
 });
-
+ */
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
 
