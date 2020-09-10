840d1cfd7f62afa0123ab312611d0775402288ba
samuel <54377301+MI7QC@users.noreply.github.com>
2020-09-09 23:45:16 -0400

    Api cellier (#10)
    * Ajout des routes api
    
    * Api bouteille
    
    * Api bouteille et correction du model
    
    * correction api route, ajout modele/controller cellier

--- app/Http/Controllers/CellierController.php
+++ app/Http/Controllers/CellierController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use App\Cellier;
+
 
 class CellierController extends Controller
 {
-    //
+    public function index() {
+
+        return Response::json(Cellier::all());
+    }
+
+  
+
+   public function show(Cellier $cellier)
+   {
+       return Response::json($cellier);
+   }
+
+  
+   public function store(Request $request)
+   {
+       return Response::json(cellier::create($request->all()), 201);
+   }
+
+ 
+   public function update(Request $request, Cellier $cellier)
+   {
+       return Response::json($cellier->update($request->all()), 200);
+   }
+  
+
+   public function destroy(Cellier $cellier)
+   {
+       $cellier->delete();
+
+       return Response::json(null, 204);
+   }
 }
