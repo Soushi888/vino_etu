52b44c5a2e302ca9a20c629db59043b2261da036
Soushi888 <Sacha.pignot@gmail.com>
2020-09-11 08:46:06 -0400

    Ajout d'un champs default_cellier_id

--- database/migrations/2018_08_12_000000_create_users_table.php
+++ database/migrations/2018_08_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->foreignId("adresse_id")->nullable()->references("id")->on("adresses");
+            $table->foreignId("default_cellier_id")->nullable()->references("id")->on("celliers");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
