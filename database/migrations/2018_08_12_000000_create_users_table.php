7c0cc7a14051f4b713710906feaaf55e2b5f5d29
Soushi888 <info@sacha-pignot.com>
2020-09-14 18:37:25 -0400

    Suppression table adresses

--- database/migrations/2018_08_12_000000_create_users_table.php
+++ database/migrations/2018_08_12_000000_create_users_table.php
@@ -15,13 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-//            $table->string('prenom');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
-            $table->foreignId("adresse_id")->nullable()->references("id")->on("adresses");
             $table->bigInteger("cellier_id")->nullable();
             $table->timestamp('email_verified_at')->nullable();
-//            $table->string('type');
+            $table->string('type');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
