ac0b11eaa6ed29a61d964cfc620859a19c20c916
Soushi888 <Sacha.pignot@gmail.com>
2020-10-01 07:22:45 -0400

    Hardcodage d'utilisateur test pour faciliter la connection

--- database/migrations/2018_08_12_000000_create_users_table.php
+++ database/migrations/2018_08_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -27,6 +29,17 @@ class CreateUsersTable extends Migration
         // Génère des données de test
         factory(App\User::class, 15)->create();
 
+        // Génère des comptes utilisateurs et administrateur avec lesquels se connecter
+        $user = User::find(1);
+        $user->email = "user@test.com";
+        $user->password = Hash::make("12345678");
+        $user->save();
+
+        $user = User::find(2);
+        $user->email = "admin@test.com";
+        $user->password = Hash::make("12345678");
+        $user->type = "admin";
+        $user->save();
     }
 
     /**
