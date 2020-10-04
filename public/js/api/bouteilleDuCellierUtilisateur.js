e66eab1a31ad1bd87eb57423d92b7bbe2405a1e9
enix426 <58696683+enix426@users.noreply.github.com>
2020-10-04 15:31:56 -0400

    mise a jour de la page accuei

---
+++ public/js/api/bouteilleDuCellierUtilisateur.js
@@ -0,0 +1,132 @@
+
+    let eDivIndex = document.querySelector(".container-index");    
+    let listCelliers = document.querySelector("#listCelliers h1");
+    let eUl = document.createElement("ul");
+    let idUtilisateur = document.getElementById("idUtilisateur").value
+    let userApi = new User;
+    userApi.showCellier(idUtilisateur).then((data => {
+    console.log(data.length);
+        if(data.length === 0){
+            creeCellier();
+            location.reload();
+        }else{            
+            data.map(cellier => {
+                bouteilles(cellier.id)
+        })
+        }
+    }))
+
+
+function creeCellier(){
+    let celliers = new Cellier();
+        let cellier = {
+            nom: "cellier",
+            user_id: idUtilisateur,
+        };
+        celliers.store(cellier);
+}
+
+    function bouteilles(evt) {        
+       
+        let idCellier = evt;
+        console.log(idCellier);
+        let accueille = document.getElementById("accueille")
+            console.log(accueille);
+            let containerBouteille = document.createElement("div");
+            containerBouteille.setAttribute("class","container_bouteille")
+            let eTable = document.createElement("table");
+            accueille.appendChild(containerBouteille).appendChild(eTable);
+            var reponse = fetch("http://vino-etu/api/affichageDetails/"+idCellier);
+            var reponseJson = reponse.then(function(res){
+                return res.json();
+            });
+            reponseJson.then(function(reponse){
+                        console.log("je suis dans le fetch de bouteille",reponse);
+                        for(let i =0; i<reponse.length; i++){
+                            console.log(reponse[i].pays)
+
+                            let eTr1 = document.createElement("tr");
+                            let eDivImg = document.createElement("div");
+                            eDivImg.setAttribute("class","img_bouteille");
+                            eTd1 = document.createElement("td");
+                            eImg = document.createElement("img");
+                            eImg.setAttribute("class","grosseur_img")
+                            eImg.setAttribute("src",reponse[i].url_image);
+                            eImg.setAttribute("alt","bouteille");
+                            eTable.appendChild(eTr1).appendChild(eDivImg).appendChild(eTd1).appendChild(eImg);
+                            eTd2 = document.createElement("td");
+                            ePNom = document.createElement("p");
+                            eTextNom = document.createTextNode("Nom: "+reponse[i].nom);
+                            ePNom.appendChild(eTextNom);
+                            ePQuantite = document.createElement("p");
+                            eTextQuantite = document.createTextNode("Quantite: "+reponse[i].quantite);
+                            ePQuantite.appendChild(eTextQuantite);
+                            ePPays = document.createElement("p");
+                            eTextPays = document.createTextNode("Pays: "+reponse[i].pays);
+                            ePPays.appendChild(eTextPays);
+                            ePType = document.createElement("p");
+                            eTextType = document.createTextNode("Type: "+reponse[i].type);
+                            ePType.appendChild(eTextType);
+                            ePMillesime = document.createElement("p");
+                            eTextMillesime = document.createTextNode("Millesime: "+reponse[i].millesime);
+                            ePMillesime.appendChild(eTextMillesime);
+                           eLien = document.createElement("a");                            
+                           eLien.setAttribute("href",reponse[i].url_saq);
+                           eLien.setAttribute("alt","lien ver la bouteille "+reponse[i].nom+"du site SAQ");
+                           eTextSaq = document.createTextNode("Voir SAQ");
+                           eLien.appendChild(eTextSaq);                     
+                           eTr1.appendChild(eTd2);
+                           eTd2.appendChild(ePNom);
+                           eTd2.appendChild(ePQuantite);
+                           eTd2.appendChild(ePPays);
+                           eTd2.appendChild(ePType);
+                           eTd2.appendChild(ePMillesime);
+                           eTd2.appendChild(eLien);
+                           eDivBouton = document.createElement("div");
+                           eDivBouton.setAttribute("class", "btn_bouteille");
+                           eBoutonModifier = document.createElement("button");
+                           eBoutonModifier.setAttribute("class","btn btn-modifier inline");
+                           eBoutonModifier.setAttribute("btn","modifier_"+reponse.code_saq);
+                           eTextModifier = document.createTextNode("Modifier")
+                           eBoutonModifier.appendChild(eTextModifier);
+                           eBoutonAjouter = document.createElement("button");
+                           eBoutonAjouter.setAttribute("class","btn btn-ajouter inline");
+                           eBoutonAjouter.setAttribute("btn","ajouter_"+reponse.code_saq);
+                           eTextAjouter = document.createTextNode("Ajouter")
+                           eBoutonAjouter.appendChild(eTextAjouter);
+                           eBoutonBoire = document.createElement("button");
+                           eBoutonBoire.setAttribute("class","btn btn-boire inline");
+                           eBoutonBoire.setAttribute("btn","boire_"+reponse.code_saq);                           
+                           eTextBoire = document.createTextNode("Boire")
+                           eBoutonBoire.appendChild(eTextBoire);
+                           eDivBouton.appendChild(eBoutonModifier);
+                           eDivBouton.appendChild(eBoutonAjouter);
+                           eDivBouton.appendChild(eBoutonBoire);
+                            eTd2.appendChild(eDivBouton);
+                        }
+/*
+<table>
+    <tr>
+        <div class="img_bouteille">
+        <td>
+            <img src="${data.url_image}" alt="bouteille" witdh="150" height="225">
+        </td>
+        </div>
+        <td>
+        <p>Nom : <b>${data.nom}</b></p>
+        <p>Pays : <b>${data.pays}</b></p>
+        <p>Type : <b>${getType(data.type_id)}</b></p>
+        <a href="${data.url_saq}">Voir SAQ</a>
+        <div class="btn_bouteille">
+        <button class="btn btn-modifier inline" btn="modifier_${data.code_saq}">Modifier</button>
+        <button class="btn btn-ajouter inline" btn="ajouter_${data.code_saq}">Ajouter</button>
+        <button class="btn btn-boire inline" btn="boire_${data.code_saq}">Boire</button>
+        </div>
+    </td>
+    </tr>
+</table>
+*/
+                    })
+                    
+    }
+
--- resources/views/accueil_utilisateur.blade.php
+++ resources/views/accueil_utilisateur.blade.php
@@ -7,12 +7,9 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=cyrillic"
           rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href={{ asset("css/style.css") }}>
-    <script src={{asset("js/api/Bouteille.js")}}></script>
-    <script src={{asset("js/api/CellierBouteille.js")}}></script>
+    <link rel="stylesheet" href={{ asset("css/style.css") }}>   
     <script src={{asset("js/api/User.js")}}></script>
-    <script src={{asset("js/functions.js")}}></script>
-    <script src={{asset("js/api/Cellier.js")}}></script>
+    
     <script src={{asset("js/modal.js")}}></script>
     <title></title>
 </head>
@@ -33,7 +30,7 @@
             <a href="#" class="logo_accueille"><img src={{ asset("img/logo_vino.png") }} alt="vino"></a>
             <h2 class="slogan">Un petite verre de vino?</h2>
         </div>
-        <aside>
+        <aside  id="accueille">
             <nav class="header-nav accueille">
                 <a class="header-nav-link active accueille" href="{{ route("mon_compte") }}">Mon compte</a>
                 <a class="header-nav-link active accueille" href="{{ route("ajouter_bouteille") }}">Ajouter une bouteille au
@@ -41,64 +38,13 @@
                 <a class="header-nav-link" href="{{route("logout")}}"><i class="fa fa-sign-out fa-2x" aria-hidden="true"></i></a>
             </nav>
 
-{{--            <div class="message-bienvenu"><h2>Cellier : <span id="nom_cellier"></span></h2></div>--}}
+           <div class="message-bienvenu"><h2>Votre cellier</h2></div>
 
-            <div class="affichaListeBouteille"></div>
-            </div>
         </aside>
     </div>
 </div>
 <footer>2020 Vino | Group 1</footer>
 
 </body>
-<script>
-    let eDivIndex = document.querySelector(".container-index");
-    eDivIndex.style.visibility = "hidden";
-    let listCelliers = document.querySelector("#listCelliers h1");
-    let eUl = document.createElement("ul");
-    let idUtilisateur = document.getElementById("idUtilisateur").value
-    let userApi = new User;
-    userApi.showCellier(idUtilisateur).then((data => {
-    console.log(data.length);
-        if(data.length === 0){
-            creeCellier();
-            location.reload();
-        }else{            
-            data.map(cellier => {
-                bouteilles(cellier.id)
-        })
-        }
-    }))
-
-
-function creeCellier(){
-    let celliers = new Cellier();
-        let cellier = {
-            nom: "cellier",
-            user_id: idUtilisateur,
-        };
-        celliers.store(cellier);
-}
-
-    function bouteilles(evt) {
-        
-        let eDivIndex = document.querySelector(".container-index");
-        eDivIndex.style.visibility = "visible";
-        let listCelliers = document.querySelector("#listCelliers");
-        listCelliers.innerHTML = "";
-        let idCellier = evt;
-        console.log(idCellier);
-        let messageBienvenu = document.getElementById("message-bienvenu h2");
- 
-                   
-            var reponse = fetch("http://vino-etu/api/affichageDetails/"+idCellier);
-            var reponseJson = reponse.then(function(res){
-                return res.json();
-            });
-            reponseJson.then(function(reponse){
-                        console.log("je suis dans le fetch de bouteille",reponse)
-                    })
-    }
-
-</script>
+<script src={{asset("js/api/bouteilleDuCellierUtilisateur.js")}}></script>
 </html>
